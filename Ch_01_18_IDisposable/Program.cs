0b976618997ba0b96bbea58bd28416128e136245
Mark Kim <Mark Kim>
2017-02-11 01:08:02 +0900

    Ch-01-18: IDisposable

---
+++ Ch_01_18_IDisposable/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace Ch_01_18_IDisposable
+{
+    class CWC : IDisposable
+    {
+        private TextWriter writer;
+
+        public void Create()
+        {
+            writer = File.CreateText("sample.txt");
+        }
+
+        public void Close()
+        {
+            Dispose();
+        }
+
+        public void Write()
+        {
+            try
+            {
+                throw new ApplicationException("Sample Exception");
+            }
+            catch (Exception)
+            {
+                Close();
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (writer != null)
+                writer.Close();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var cwc = new CWC())
+                {
+                    cwc.Create();
+                    cwc.Write();
+                    cwc.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
