6b71a717eba6b77792abe8e2ca14104c7f078135
Mark Kim <Mark Kim>
2017-02-10 19:51:33 +0900

    Ch-01-07: Async loop

--- Ch_01_07_Async_loop/Program.cs
+++ Ch_01_07_Async_loop/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Ch_01_07_Async_loop
 {
     class Program
     {
+        private static async Task countDown()
+        {
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+                await Task.Delay(500);
+            }
+        }
         static void Main(string[] args)
         {
+            var a = countDown();
+            var b = countDown();
+
+            Console.WriteLine("Doing...");
+            Task.WaitAll(a, b);
+            Console.WriteLine("All done.");
         }
     }
 }
