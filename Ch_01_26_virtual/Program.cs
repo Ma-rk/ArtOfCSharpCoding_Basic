db292d43af66d72509e83786bc39fc8deeb1cb51
Mark Kim <Mark Kim>
2017-02-11 08:36:36 +0900

    Ch-01-26: virtual

---
+++ Ch_01_26_virtual/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ch_01_26_virtual
+{
+    class Product
+    {
+        private Func<int, int> exp;
+
+        public int CalcPrc(int basePrc)
+        {
+            return this.exp(basePrc);
+        }
+
+        public Product(Func<int, int> exp)
+        {
+            this.exp = exp;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product((n) => n * 8 / 10);
+            Console.WriteLine(p.CalcPrc(100));
+        }
+    }
+}
