f917d9db34b62edbff95dd066886ffdb594bc506
Mark Kim <Mark Kim>
2017-02-14 10:16:11 +0900

    Ch-02-12: resource

---
+++ Ch_02_12_resource/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ch_02_12_resource
+{
+    class Program
+    {
+        // before
+        //private static double Calc100Avg(int[] ary)
+        //{
+        //    return ary.Take(100).Average();
+        //}
+
+        // improved
+        private static double Calc100Avg(IEnumerable<int> ary)
+        {
+            return ary.Take(100).Average();
+        }
+
+        static void Main(string[] args)
+        {
+            // before
+            int[] ary = Enumerable.Range(0, 10000).ToArray();
+            Console.WriteLine(Calc100Avg(ary));
+
+            // improved
+            IEnumerable<int> ary2 = Enumerable.Range(0, 10000);
+            Console.WriteLine(Calc100Avg(ary2));
+        }
+    }
+}
